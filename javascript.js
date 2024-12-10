c0e91c530780f92e82870b184186ffe2205bf6ca
Ben Warr <warrbenjamin@gmail.com>
2024-12-10 09:46:56 +0000

    Files

---
+++ javascript.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", function () {
+    fetch('data.json')
+        .then(response => response.json())
+        .then(data => {
+            const projectsContainer = document.querySelector('.projects-and-skills');
+
+            data.projects.forEach(project => {
+                const projectCard = document.createElement('div');
+                projectCard.classList.add('project');
+
+                projectCard.innerHTML = `
+                    <h4>${project.title}</h4>
+                    <div class="content">
+                        <div class="desc">
+                            <p>${project.description}</p>
+                        </div>
+                    </div>
+                `;
+
+                projectsContainer.appendChild(projectCard);
+            });
+        })
+        .catch(error => console.error('Error fetching the projects:', error));
+});
